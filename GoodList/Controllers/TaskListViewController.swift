5f7a9f77ae656a924e7b8c91865b122c62017b4b
nikoagge <nikoagge@gmail.com>
2023-05-14 12:27:18 +0300

    Wrong commit message before, right now setup of TaskListViewController

--- GoodList/Controllers/TaskListViewController.swift
+++ GoodList/Controllers/TaskListViewController.swift
@@ -5,4 +5,22 @@
 //  Created by Nikos Aggelidis on 14/5/23.
 //
 
-import Foundation
+import UIKit
+
+final class TaskListViewController: UIViewController {
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+}
+
+// MARK: UITableViewDataSource, UITableViewDelegate
+extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 13
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell
+    }
+}
