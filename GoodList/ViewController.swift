95b8bbc1d81da27b46483c4feb05c8aec1502c17
nikoagge <n.aggelidis@m-stat.gr>
2023-05-14 12:06:19 +0300

    Initial Commit

---
+++ GoodList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodList
+//
+//  Created by Nikos Aggelidis on 14/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
