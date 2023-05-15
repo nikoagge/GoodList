4297f70d53b173607887344c6fcfcf23436ac04e
nikoagge <nikoagge@gmail.com>
2023-05-15 17:09:46 +0300

    Displaying tasks that I add

--- GoodList/Controllers/TaskListViewController.swift
+++ GoodList/Controllers/TaskListViewController.swift
@@ -50,11 +50,12 @@ final class TaskListViewController: UIViewController {
 // MARK: UITableViewDataSource, UITableViewDelegate
 extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 13
+        return filteredTasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        cell.textLabel?.text = filteredTasks[indexPath.row].title
         
         return cell
     }
@@ -66,15 +67,23 @@ private extension TaskListViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    func tableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
     func filterTasks(by priority: Priority?) {
         if priority == nil {
             filteredTasks = tasks.value
+            tableView.reloadData()
         } else {
             tasks.map { tasks in
                 return tasks.filter { $0.priority == priority! }
             }.subscribe(onNext: { [weak self] tasks in
                 guard let self = self else { return }
                 self.filteredTasks = tasks
+                self.tableView.reloadData()
             }).disposed(by: disposeBag)
         }
     }
